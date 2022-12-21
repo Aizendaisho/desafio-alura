ced4ff0d3551932466a9081ff6b97ce4f81f6679
Aizendaisho <96481537+Aizendaisho@users.noreply.github.com>
2022-12-20 20:42:18 -0400

    g

--- index.js
+++ index.js
@@ -4,18 +4,31 @@
 const encrintar = document.querySelector(".boton-1")
 const descrintar = document.querySelector(".boton-2")
 const texto = document.querySelector(".textarea-1")
+const contenedor = document.querySelector(".contenedor-imagen")
 
 encrintar.addEventListener("click", ()=>{
     let encri = btoa(quitarAcento(texto.value.toLowerCase()))
+    contenedor.innerHTML = `
+    
+    <p>${encri}</p>
+    
+    `
     console.log(encri)
 })
 descrintar.addEventListener("click", ()=>{
-    console.log(atob(texto.value))
     let encri = atob(texto.value)
+    contenedor.innerHTML = `
+
+    <p>${encri}</p>
+    
+    `
     console.log(encri)
+    console.log(atob(texto.value))
 })
 
 const quitarAcento =(string)=>{
     string = string.normalize("NFD").replace(/[^a-zA-Z0-9]/g, " ");
     return string
 }
+
+console.log(contenedor)
