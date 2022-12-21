693458f86f674eaf5168167554bd76ce23d7c495
Araldi Garcia <96481537+Aizendaisho@users.noreply.github.com>
2022-12-21 14:03:26 -0400

    challenge terminado, solo falta la parte responsiv

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-//// funcion para encrintar btoa() y desencriptar atob()
 'use strict';
 
 const encrintar = document.querySelector(".boton-1")
 const descrintar = document.querySelector(".boton-2")
 const texto = document.querySelector(".textarea-1")
 const contenedor = document.querySelector(".contenedor-imagen")
+const textArea = document.createElement("textarea");
+const buttonCopiar = document.createElement("button");
+const contenedorEncriptado = document.createElement("div");
+
+textArea.classList.add("textArea-generado")
+buttonCopiar.innerHTML = "Copiar"
+buttonCopiar.classList.add("boton-copia")
+contenedorEncriptado.classList.add("contenedor-encriptado")
+contenedorEncriptado.appendChild(textArea)
+contenedorEncriptado.appendChild(buttonCopiar)
+textArea.id ="texto"
+
+
+
+buttonCopiar.addEventListener("click",()=>{
+    const text =document.getElementById("texto")
+    console.log("hola")
+    text.select()
+    document.execCommand("copy")
+    text.blur()
+    alert("texto copiado")
+    
+    
+})
 
 encrintar.addEventListener("click", ()=>{
     let encri = quitarAcento(texto.value.toLowerCase())
@@ -13,11 +36,9 @@ encrintar.addEventListener("click", ()=>{
     encri = encri.replace(/a/img,"ai")
     encri = encri.replace(/o/img,"ober")
     encri = encri.replace(/u/img,"ufat")
-    contenedor.innerHTML = `
-    
-    <p>${encri}</p>
-    
-    `;
+    textArea.innerHTML = encri
+    contenedor.innerHTML= ""
+    contenedor.appendChild(contenedorEncriptado);
     console.log(encri)
  
 })
@@ -28,11 +49,9 @@ descrintar.addEventListener("click", ()=>{
     encri = encri.replace(/ai/img,"a")
     encri = encri.replace(/ober/img,"o")
     encri = encri.replace(/ufat/img,"u")
-    contenedor.innerHTML = `
-
-    <p>${encri}</p>
-    
-    `;
+    textArea.innerHTML = encri
+    contenedor.innerHTML= ""
+    contenedor.appendChild(contenedorEncriptado);
 
 })
 
@@ -42,6 +61,7 @@ const quitarAcento =(string)=>{
 }
 
 // const remplazo= (string, letra, frase) =>{
+
 //     const vari = `/${letra}/img`
 //     let encri = string
 //     console.log(vari)
@@ -49,4 +69,6 @@ const quitarAcento =(string)=>{
 //     return encri = encri.replace(vari,`${frase}`)
 
 // }
+
+
 
