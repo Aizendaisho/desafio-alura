cfc8d80b0aec1d718a36babb648443998f140c6a
Aizendaisho <96481537+Aizendaisho@users.noreply.github.com>
2022-12-22 10:44:20 -0400

    codigo refactorizado

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
-'use strict';
-
-const encrintar = document.querySelector(".boton-1")
-const descrintar = document.querySelector(".boton-2")
-const texto = document.querySelector(".textarea-1")
-const contenedor = document.querySelector(".contenedor-imagen")
-const textArea = document.createElement("textarea");
-const buttonCopiar = document.createElement("button");
-const contenedorEncriptado = document.createElement("div");
-
-textArea.classList.add("textArea-generado")
-buttonCopiar.innerHTML = "Copiar"
-buttonCopiar.classList.add("boton-copia")
-contenedorEncriptado.classList.add("contenedor-encriptado")
-contenedorEncriptado.appendChild(textArea)
-contenedorEncriptado.appendChild(buttonCopiar)
-textArea.id ="texto"
-textArea.cols= 30;
-textArea.rows =10;
-buttonCopiar.addEventListener("click",()=>{
-    const text =document.getElementById("texto")
-    console.log("hola")
-    text.select()
-    document.execCommand("copy")
-    text.blur()
-    alert("texto copiado")
-    
-    
-})
-
-encrintar.addEventListener("click", ()=>{
-    let encri = quitarAcento(texto.value.toLowerCase())
-    encri = encri.replace(/e/img,"enter")
-    encri = encri.replace(/i/img,"imes")
-    encri = encri.replace(/a/img,"ai")
-    encri = encri.replace(/o/img,"ober")
-    encri = encri.replace(/u/img,"ufat")
-    textArea.innerHTML = encri
-    contenedor.innerHTML= ""
-    contenedor.appendChild(contenedorEncriptado);
-    console.log(encri)
- 
-})
-descrintar.addEventListener("click", ()=>{
-    let encri =  quitarAcento(texto.value.toLowerCase())
-    encri = encri.replace(/enter/img,"e")
-    encri = encri.replace(/imes/img,"i")
-    encri = encri.replace(/ai/img,"a")
-    encri = encri.replace(/ober/img,"o")
-    encri = encri.replace(/ufat/img,"u")
-    textArea.innerHTML = encri
-    contenedor.innerHTML= ""
-    contenedor.appendChild(contenedorEncriptado);
-
-})
-
-const quitarAcento =(string)=>{
-    string = string.normalize("NFD").replace(/[^a-zA-Z0-9]/g, " ");
+const ENCRYPT_MAPPINGS = [
+    { from: "e", to: "enter" },
+    { from: "i", to: "imes" },
+    { from: "a", to: "ai" },
+    { from: "o", to: "ober" },
+    { from: "u", to: "ufat" },
+  ];
+  
+  const DECRYPT_MAPPINGS = [
+    { from: "enter", to: "e" },
+    { from: "imes", to: "i" },
+    { from: "ai", to: "a" },
+    { from: "ober", to: "o" },
+    { from: "ufat", to: "u" },
+  ];
+  
+  const removeAccents = (string) => {
+        string = string.normalize("NFD").replace(/[^a-zA-Z0-9]/g, " ");
     return string
-}
+  };
+  
+  const copyText = (text) => {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Texto copiado al portapapeles");
+  };
+  
+  const encrypt = (text) => {
+    let encrypted = removeAccents(text.toLowerCase());
+    ENCRYPT_MAPPINGS.forEach((mapping) => {
+      encrypted = encrypted.replace(new RegExp(mapping.from, "img"), mapping.to);
+    });
+    return encrypted;
+  };
+  
+  const decrypt = (text) => {
+    let decrypted = removeAccents(text.toLowerCase());
+    DECRYPT_MAPPINGS.forEach((mapping) => {
+      decrypted = decrypted.replace(new RegExp(mapping.from, "img"), mapping.to);
+    });
+    return decrypted;
+  };
+  
+  const encrintar = document.querySelector(".boton-1");
+  const descrintar = document.querySelector(".boton-2");
+  const texto = document.querySelector(".textarea-1");
+  const contenedor = document.querySelector(".contenedor-imagen");
+  const textArea = document.createElement("textarea");
+  const buttonCopiar = document.createElement("button");
+  const contenedorEncriptado = document.createElement("div");
+  
+  textArea.classList.add("textArea-generado");
+  buttonCopiar.innerHTML = "Copiar";
+  buttonCopiar.classList.add("boton-copia");
+  contenedorEncriptado.classList.add("contenedor-encriptado");
+  contenedorEncriptado.appendChild(textArea);
+  contenedorEncriptado.appendChild(buttonCopiar);
+  textArea.id = "texto";
+  textArea.cols = 30;
+  textArea.rows = 10;
+  
+  buttonCopiar.addEventListener("click", () => {
+    copyText(textArea.value);
+  });
+  
+  encrintar.addEventListener("click", () => {
+    console.log(texto)
+    const encrypted = encrypt(texto.value)
+    console.log(encrypted);
+    textArea.innerHTML = encrypted
+    contenedor.innerHTML= ""
+    contenedor.appendChild(contenedorEncriptado);
+    
+  
+  })
 
-// const remplazo= (string, letra, frase) =>{
-
-//     const vari = `/${letra}/img`
-//     let encri = string
-//     console.log(vari)
-//     console.log(frase)
-//     return encri = encri.replace(vari,`${frase}`)
-
-// }
-
-
+  descrintar.addEventListener("click", () => {
+    console.log(texto)
+    const encrypted = decrypt(texto.value)
+    console.log(encrypted);
+    textArea.innerHTML = encrypted
+    contenedor.innerHTML= ""
+    contenedor.appendChild(contenedorEncriptado);
+    
+  
+  })
 
